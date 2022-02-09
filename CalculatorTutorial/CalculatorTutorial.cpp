75a7bdcb1c2bfd9a50ba13138f319deac501eea4
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-02-09 21:40:03 +0600

    подключение библиотек тбб

--- CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -1,22 +1,12 @@
 ﻿
 #include <iostream>
-#include <thread>
-using namespace std;
-void hello() {
-	cout << "Hello Concurent World \n";
-}
-int main()
-{
-	double i = 0;
-	double j = 0;
-	while (true)
-	{
-		i++;
-		if (i > 2 * 1e8) {
-			i = 0;
-			cout << j << "\n";
-			j++;
-		}
-	}
+#include <oneapi/tbb.h>
 
+
+int main() {
+    tbb::parallel_invoke(
+        []() { std::cout << " Hello " << std::endl; },
+        []() { std::cout << " TBB! " << std::endl; }
+    );
+    return 0;
 }
