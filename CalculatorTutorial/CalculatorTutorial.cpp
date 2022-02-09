ca775f36e53ad8fcece2a97b39cdf6572b9f47e6
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-02-09 11:48:38 +0600

    первый поток

--- CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -2,40 +2,16 @@
 //
 
 #include <iostream>
-#include "Calculator.h"
+#include <thread>
 using namespace std;
+void hello() {
+    cout <<  "Hello Concurent World \n";
+}
 int main()
 {
-    double x = 0.0;
-    double y = 0.0;
-    double result = 0.0;
-    char oper = '+';    
-
-    cout << "Calculator Console Application" << endl << endl;
-    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
-        << endl;
-    Calculator c;
-    while (true)
-    {
-        cin >> x >> oper >> y;
-        if (oper == '/' && y == 0) {
-            cout << "Divide by zero \n";
-            continue;
-        }
-        result = c.Calculate(x, oper, y);
-        cout << "Result is: " << result << endl;
-        // Тестовый комментарий
-    }
-    return 0;
+    thread t(hello);
+    thread t2(hello);
+    t.join();
+    t2.join();
+    
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
